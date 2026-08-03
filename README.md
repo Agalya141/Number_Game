4c136ed5a8e1bf205727b5da589051b0561d795c
Agalya141 <agal4312@gmail.com>
2026-08-03 19:16:08 +0530

    Created Number Guessing Game using Html,Css,JavaScript

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Number_Game
+This is a Number Guessing Game 
+Guess the Correct number to win 
+The number is Chosen automatically
+
+This Game is created using html css javascript 
--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-
+var guessnumber = document.getElementById("guessnumber");
+var submit = document.getElementById("button");
+var result = document.getElementById("result");
+var score = document.getElementById("totalscore");
+var randomnumber = Math.floor(Math.random() * 10) + 1;
+var totalscore = 10;
+function check(){
+    var userguess = guessnumber.value;
+    if(userguess == randomnumber){
+        result.innerHTML = "Congratulations! You Won!";
+        alert("You Won The Game!")
+        console.log("Right")
+    }
+    else{
+        console.log("Wrong")
+        totalscore = totalscore - 1;
+        result.innerHTML = "Sorry! You Lost!"
+        score.innerHTML = "Score: " + totalscore;
+    }
+    if(totalscore == 0){
+        alert("Game Over! You have no more attempts left.");
+    }
+    
+}
